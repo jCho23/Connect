f3682d1f76e0735379ee31b71a8215c7509eb242
JuneCho23 <junecho.ucb@gmail.com>
2017-03-04 18:33:44 -0800

    Test Automation Scripts for Connect

---
+++ Connect/Tests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Android;
+using Xamarin.UITest.Queries;
+
+namespace Connect
+{
+	[TestFixture]
+	public class Tests
+	{
+		AndroidApp app;
+
+		[SetUp]
+		public void BeforeEachTest()
+		{
+			// TODO: If the Android app being tested is included in the solution then open
+			// the Unit Tests window, right click Test Apps, select Add App Project
+			// and select the app projects that should be tested.
+			app = ConfigureApp
+				.Android
+				// TODO: Update this path to point to your Android app and uncomment the
+				// code if the app is not included in the solution.
+				.ApkFile("/Users/junecho/Desktop/Connect.apk")
+				.StartApp();
+		}
+
+		[Test]
+		public void AppLaunches()
+		{
+
+			app.WaitForElement(x => x.Text(("Use my phone number")));
+			app.Flash(x => x.Text("Use my phone number"));
+			app.Tap(x => x.Text("Use my phone number"));
+			app.Screenshot("Phone Number Field");
+
+			app.Tap(x => x.Id("dgts__countryCode"));
+			app.Screenshot("Country Code Field");
+
+			app.ScrollDownTo((x => x.Id("text1").Text("United States +1")));
+			app.Tap(x => x.Id("text1").Text("United States +1"));
+			app.Screenshot("Scrolling down to United States Field");
+
+			app.EnterText("4087188819");
+			app.Screenshot("Phone number Field");
+			app.Tap((x => x.Text("Continue")));
+
+
+
+
+
+		}
+	}
+}
